8e2987283226ff4e821293a24c2033b1e7665fd2
Chris Z <ckz8780@gmail.com>
2020-12-16 00:25:22 +0000

    Added boilerplate

--- filter.js
+++ filter.js
@@ -1,1 +1,50 @@
+// Simple Filtering
+const people = [
+  {
+    name: 'Michael',
+    age: 23,
+  },
+  {
+    name: 'Lianna',
+    age: 16,
+  },
+  {
+    name: 'Paul',
+    age: 18,
+  },
+];
 
+
+// Complex Filtering
+const students = [
+  {
+    id: 1,
+    name: 'Mark',
+    profession: 'Developer',
+    skills: [
+      { name: 'javascript', yrsExperience: 1 },
+      { name: 'html', yrsExperience: 5 },
+      { name: 'css', yrsExperience: 3 },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Ariel',
+    profession: 'Developer',
+    skills: [
+      { name: 'javascript', yrsExperience: 0 },
+      { name: 'html', yrsExperience: 4 },
+      { name: 'css', yrsExperience: 2 },
+    ]
+  },
+  {
+    id: 3,
+    name: 'Jason',
+    profession: 'Designer',
+    skills: [
+      { name: 'javascript', yrsExperience: 1 },
+      { name: 'html', yrsExperience: 1 },
+      { name: 'css', yrsExperience: 5 },
+    ]
+  },
+];
